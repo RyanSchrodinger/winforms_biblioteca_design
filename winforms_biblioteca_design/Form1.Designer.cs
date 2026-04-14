5108138d7e62daf2dfd903fc842564a0f5dac230
Ryan <ronaldsagan000@gmail.com>
2026-04-14 18:54:59 -0300

    style: definir as cores que serao usadas

--- winforms_biblioteca_design/Form1.Designer.cs
+++ winforms_biblioteca_design/Form1.Designer.cs
@@ -28,13 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tcControle = new MaterialSkin.Controls.MaterialTabControl();
+            this.tpPrincipal = new System.Windows.Forms.TabPage();
+            this.tpUsuarios = new System.Windows.Forms.TabPage();
+            this.tpLivros = new System.Windows.Forms.TabPage();
+            this.tpFuncionarios = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
-            this.panel3.SuspendLayout();
+            this.tcControle.SuspendLayout();
+            this.tpFuncionarios.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(64)))), ((int)(((byte)(70)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(217, 618);
+            this.panel2.TabIndex = 0;
             // 
             // panel1
             // 
@@ -42,61 +58,106 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.tcControle);
             this.panel1.Controls.Add(this.panel2);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(16, 15);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(997, 502);
+            this.panel1.Size = new System.Drawing.Size(1329, 618);
             this.panel1.TabIndex = 0;
             // 
-            // panel2
+            // tcControle
             // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(231)))), ((int)(((byte)(221)))));
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(156, 502);
-            this.panel2.TabIndex = 0;
+            this.tcControle.Controls.Add(this.tpPrincipal);
+            this.tcControle.Controls.Add(this.tpUsuarios);
+            this.tcControle.Controls.Add(this.tpLivros);
+            this.tcControle.Controls.Add(this.tpFuncionarios);
+            this.tcControle.Depth = 0;
+            this.tcControle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tcControle.Location = new System.Drawing.Point(217, 0);
+            this.tcControle.MouseState = MaterialSkin.MouseState.HOVER;
+            this.tcControle.Name = "tcControle";
+            this.tcControle.SelectedIndex = 0;
+            this.tcControle.Size = new System.Drawing.Size(1112, 618);
+            this.tcControle.TabIndex = 1;
             // 
-            // panel3
+            // tpPrincipal
             // 
-            this.panel3.Controls.Add(this.panel4);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel3.Location = new System.Drawing.Point(156, 0);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(841, 502);
-            this.panel3.TabIndex = 1;
+            this.tpPrincipal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.tpPrincipal.Location = new System.Drawing.Point(4, 25);
+            this.tpPrincipal.Name = "tpPrincipal";
+            this.tpPrincipal.Padding = new System.Windows.Forms.Padding(3);
+            this.tpPrincipal.Size = new System.Drawing.Size(1104, 589);
+            this.tpPrincipal.TabIndex = 0;
+            this.tpPrincipal.Text = "Tela Principal";
             // 
-            // panel4
+            // tpUsuarios
             // 
-            this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(231)))), ((int)(((byte)(221)))));
-            this.panel4.Location = new System.Drawing.Point(27, 47);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(797, 113);
-            this.panel4.TabIndex = 2;
+            this.tpUsuarios.Location = new System.Drawing.Point(4, 25);
+            this.tpUsuarios.Name = "tpUsuarios";
+            this.tpUsuarios.Padding = new System.Windows.Forms.Padding(3);
+            this.tpUsuarios.Size = new System.Drawing.Size(1104, 589);
+            this.tpUsuarios.TabIndex = 1;
+            this.tpUsuarios.Text = "Usuarios";
+            this.tpUsuarios.UseVisualStyleBackColor = true;
+            // 
+            // tpLivros
+            // 
+            this.tpLivros.Location = new System.Drawing.Point(4, 25);
+            this.tpLivros.Name = "tpLivros";
+            this.tpLivros.Padding = new System.Windows.Forms.Padding(3);
+            this.tpLivros.Size = new System.Drawing.Size(1104, 589);
+            this.tpLivros.TabIndex = 2;
+            this.tpLivros.Text = "Livros";
+            this.tpLivros.UseVisualStyleBackColor = true;
+            // 
+            // tpFuncionarios
+            // 
+            this.tpFuncionarios.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.tpFuncionarios.Controls.Add(this.pictureBox1);
+            this.tpFuncionarios.Location = new System.Drawing.Point(4, 25);
+            this.tpFuncionarios.Name = "tpFuncionarios";
+            this.tpFuncionarios.Padding = new System.Windows.Forms.Padding(3);
+            this.tpFuncionarios.Size = new System.Drawing.Size(1104, 589);
+            this.tpFuncionarios.TabIndex = 3;
+            this.tpFuncionarios.Text = "Funcionarios";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::winforms_biblioteca_design.Properties.Resources.biblioteca;
+            this.pictureBox1.Location = new System.Drawing.Point(383, 161);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(269, 297);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1021, 526);
+            this.ClientSize = new System.Drawing.Size(1361, 647);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
+            this.tcControle.ResumeLayout(false);
+            this.tpFuncionarios.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel1;
+        private MaterialSkin.Controls.MaterialTabControl tcControle;
+        private System.Windows.Forms.TabPage tpPrincipal;
+        private System.Windows.Forms.TabPage tpUsuarios;
+        private System.Windows.Forms.TabPage tpLivros;
+        private System.Windows.Forms.TabPage tpFuncionarios;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
