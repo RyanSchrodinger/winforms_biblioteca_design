7d6e04c753c204a7eab5b953691bb6c07c1ed72a
ryan.rssantos <ryan.rssantos@OSA0716347W11-1>
2026-04-30 15:56:33 -0300

    feat: insere procedures referente a entidade usuarios

---
+++ Banco de Dados/04_ProcedureUsuario.sql
@@ -0,0 +1,58 @@
+USE BibliotecaDB
+
+-- PROCEDURE Obter Dados
+
+GO
+CREATE PROCEDURE ObterDadosUsuario
+AS
+BEGIN
+	SELECT * FROM Usuarios
+END
+GO
+
+-- PROCEDURE Deletar Usuarios
+
+GO
+CREATE PROCEDURE DeletarUsuario
+@IdExcluido INT
+AS 
+BEGIN 
+	DELETE FROM Usuarios
+	WHERE UsuarioID = @IdExcluido
+END
+GO
+
+-- PROCEDURE Inserir Usuario
+
+GO
+	CREATE PROCEDURE InserirDadosUsuarios
+		@Nome varchar(100),
+		@Email varchar(100),
+		@Telefone varchar(15)
+	AS
+	BEGIN
+		INSERT INTO Usuarios(Nome,Email,Telefone)
+		VALUES (@Nome,@Email,@Telefone)
+	END
+GO
+
+-- PROCEDURE Atualizar Usuario 
+
+GO
+    CREATE PROCEDURE AtualizarUsuarios
+    @IdSelecionado INT,
+    @Nome VARCHAR(100),
+    @Email VARCHAR(100),
+    @Telefone VARCHAR(100)
+    AS
+    BEGIN
+        UPDATE Usuarios
+        SET
+            Nome = @Nome,
+            Email = @Email,
+            Telefone = @Telefone
+        WHERE
+            UsuarioID = @IdSelecionado
+    END
+GO
+
