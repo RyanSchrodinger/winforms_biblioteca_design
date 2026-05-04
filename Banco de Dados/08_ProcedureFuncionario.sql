290d03cacf422381f73cb7ded5aa618f251e2321
Ryan <ronaldsagan000@gmail.com>
2026-05-03 21:15:54 -0300

    feat(db): insere procedure de deletar funcionario

--- Banco de Dados/08_ProcedureFuncionario.sql
+++ Banco de Dados/08_ProcedureFuncionario.sql
@@ -14,3 +14,18 @@ GO
 GO
 
 -- PROCEDURE Deletar Funcionários (cascade)
+GO
+    CREATE OR ALTER PROCEDURE DeletarFuncionario
+        @ID INT
+    AS 
+    BEGIN
+        SET NOCOUNT ON
+        BEGIN TRANSACTION 
+            DELETE FROM Requisicoes
+            WHERE FuncionarioID = @ID
+            DELETE FROM Funcionarios
+            WHERE FuncionarioID = @ID
+        COMMIT 
+    END 
+GO
+
