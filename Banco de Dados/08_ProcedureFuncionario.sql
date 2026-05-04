8a010e5044c03eaa7df108fe6cc44498bbac03fd
Ryan <ronaldsagan000@gmail.com>
2026-05-03 22:22:48 -0300

    feat(db): insere procedure de inserir funcionario

--- Banco de Dados/08_ProcedureFuncionario.sql
+++ Banco de Dados/08_ProcedureFuncionario.sql
@@ -29,3 +29,17 @@ GO
     END 
 GO
 
+-- PROCEDURE Inserir Funcionários
+
+GO
+    CREATE PROCEDURE InserirFuncionario
+    @NomeUsuario VARCHAR(100),
+    @SenhaHash VARCHAR(100),
+    @NomeCompleto VARCHAR(100),
+    @Cargo VARCHAR (100)
+    AS
+    BEGIN
+        INSERT INTO Funcionarios(NomeCompleto, NomeUsuario, SenhaHash, Cargo)
+        VALUES (@NomeCompleto,@NomeUsuario,@SenhaHash,@Cargo)
+    END
+GO
